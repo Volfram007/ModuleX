33eed4d353344d127773e23aa3e16fe65222d5bf
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-06-22 08:25:55 +0300

    Итераторы

--- Module9/task3.py
+++ Module9/task3.py
@@ -2,13 +2,16 @@
 
 class EvenNumbers:
     def __init__(self, start=0, end=1):
+        # Проверка на равенство start и end
         if start == end:
             raise ValueError('"start" и "end" числа не могут быть равны')
+        # Если start больше end, меняем их местами
         elif start > end:
             start, end = end, start
             # raise ValueError('"start" число должно быть меньше "end"')
-
+        # Убеждаемся, что start - четное число
         self.start = start if start % 2 == 0 else start - 1
+        # Убеждаемся, что end - нечетное число
         self.end = end if end % 2 == 1 else end + 1
 
     def __iter__(self):
@@ -28,5 +31,6 @@ try:
     en = EvenNumbers(10, 25)
     for i in en:
         print(i)
+# Если возникнет ошибка равенства start и end, выводим текст ошибки
 except ValueError as e:
     print(f"Ошибка: {e}")
