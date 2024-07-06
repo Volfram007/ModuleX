7dd0da3e785d5b9b7c524798c516757638213be1
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-07-06 16:01:11 +0300

    Задача "Магические здания"

--- Module5/task2.py
+++ Module5/task2.py
@@ -1,12 +1,39 @@
 class House:
-    def __init__(self):  # Инициализируем атрибуты по умолчанию
-        self.numberOfFloors = 0
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.numberOfFloors = number_of_floors
 
-    def setNewNumberOfFloors(self, floors):
-        self.numberOfFloors = floors
-        print("Количество этажей:", self.numberOfFloors)
+    def __str__(self):
+        return f'Название "{self.name}", кол-во этажей: {self.numberOfFloors}'
+
+    def __len__(self):
+        return self.numberOfFloors
 
 
-my_house = House()  # Создаем экземпляр класса House
-my_house.setNewNumberOfFloors(5)  # Устанавливаем новое количество этажей
-my_house.setNewNumberOfFloors(10)
+house1 = House('ЖК Эльбрус', 10)  # Создаем экземпляр класса
+house2 = House('ЖК Акация', 20)
+print(house1)
+print(house2)
+print(len(house1))
+print(len(house2))
+
+'''
+**Задача "Магические здания"**
+Дополните класс `House` следующими методами:
+1. `__len__(self)`: возвращает количество этажей здания (`self.number_of_floors`).
+2. `__str__(self)`: возвращает строку: "Название: <название>, кол-во этажей: <этажи>".
+
+**Пример выполнения программы:**
+```python
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)  # Название: ЖК Эльбрус, кол-во этажей: 10
+print(h2)  # Название: ЖК Акация, кол-во этажей: 20
+
+# __len__
+print(len(h1))  # 10
+print(len(h2))  # 20
+```
+'''
