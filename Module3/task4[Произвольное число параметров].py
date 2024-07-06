12eb152edade7b5c6655dd026aeddda14f1cb1db
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-07-06 15:29:58 +0300

    Задача "Однокоренные"

--- Module3/task4[Произвольное число параметров].py
+++ Module3/task4[Произвольное число параметров].py
@@ -19,3 +19,29 @@ result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 
 print(result1)
 print(result2)
+
+'''
+**Задача "Однокоренные"**
+Напишите функцию `single_root_words`, которая принимает обязательный параметр `root_word` и неограниченную 
+последовательность в параметр `*other_words`. Функция должна возвращать список `same_words`, 
+содержащий слова из `other_words`, которые включают `root_word` или содержатся в `root_word`.
+
+**Пункты задачи:**
+1. Объявите функцию `single_root_words` с параметрами `root_word` и `*other_words`.
+2. Создайте пустой список `same_words`.
+3. Переберите слова из `other_words` с помощью цикла `for`.
+4. Добавляйте слова в `same_words`, если они удовлетворяют условию задачи.
+5. Верните список `same_words`.
+
+**Пример выполнения программы:**
+```python
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)  # ['richiest', 'orichalcum', 'richies']
+print(result2)  # ['Able', 'Disable']
+```
+
+**Примечания:**
+- Регистр символов не должен влиять на проверку наличия одного слова в другом.
+- Полезные методы: `in`, `count()`, `lower()`, `upper()`.
+'''
