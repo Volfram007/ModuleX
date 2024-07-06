2e713905eeb753352677cea34be534e9bfb33307
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-07-06 15:40:15 +0300

    Задача "Пространство имен"

--- Module4/task2.py
+++ Module4/task2.py
@@ -6,3 +6,27 @@ def test_function():
 
 
 test_function()
+
+'''
+**Задача "Пространство имен"**
+1. Создайте новый проект в PyCharm.
+2. Запустите проект.
+
+**Задача:**
+1. Создайте функцию `test_function`.
+2. Внутри `test_function` создайте функцию `inner_function`, которая печатает 
+    "Я в области видимости функции test_function".
+3. Вызовите `inner_function` внутри `test_function`.
+4. Попробуйте вызвать `inner_function` вне `test_function` и посмотрите на результат выполнения программы.
+
+**Пример кода:**
+```python
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
+inner_function()  # Это вызовет ошибку
+```
+'''
