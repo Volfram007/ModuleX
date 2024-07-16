a152b350508f0381f5880b30f4e2a1b8845aa3ff
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-07-16 16:03:28 +0300

    Задание "Виртуальные окружения"

---
+++ Module11/task2[Виртуальные окружения].py
@@ -0,0 +1,11 @@
+"""
+Команда создаёт папку виртуального окружения:
+python -m venv venv
+
+Команда активации папки виртуального окружения:
+venv/Scripts/activate
+
+Обновляем pip
+python.exe -m pip install --upgrade pip
+
+"""
