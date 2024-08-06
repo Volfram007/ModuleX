3a44d3b2505772d45aefc88aab3d9770b5e878a2
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-08-06 17:29:40 +0300

    Задача "Средний баланс пользователя"

--- Module14[Библиотека для работы с данными]/task2[Выбор элементов и функции в SQL запросах].py
+++ Module14[Библиотека для работы с данными]/task2[Выбор элементов и функции в SQL запросах].py
@@ -11,7 +11,6 @@ def create_database(path):
         Раскомментировать для создания таблицы
     """
     # connect.execute('DROP TABLE IF EXISTS Users')
-    #
     # cursor.execute('''
     #     CREATE TABLE Users (
     #         id INTEGER PRIMARY KEY,
@@ -21,23 +20,18 @@ def create_database(path):
     #         Balance INTEGER NOT NULL
     #     );
     # ''')
-    #
     # # Заполняем таблицу
     # [cursor.execute('INSERT INTO Users (UserName, Email, Age, Balance) VALUES (?,?,?,?)',
     #                 (f'User{i}', f'Example{i}@gmail.com', f'{i}0', f'1000')) for i in range(1, 11)]
-    #
     # # Обновляем каждую вторую запись
     # cursor.execute('UPDATE Users SET balance=? WHERE id % 2 = 1', ('500',))
-    #
     # # Удаляем каждую третью запись
     # cursor.execute('DELETE FROM Users WHERE id % 3 = 1')
-    #
     # # Отправляем изменения в базу данных
     # connect.commit()
 
     # Удаляем пользователя с id = 6
     cursor.execute('DELETE FROM Users WHERE id = 6')
-
     # Получение среднего баланса
     cursor.execute('SELECT AVG(Balance) FROM Users')
     balances = cursor.fetchone()[0]
