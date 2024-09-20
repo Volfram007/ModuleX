a2890574fad3302a068be7af4a9f77e9c20ea790
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-09-20 18:00:51 +0300

    Задача "Шаблонизатор Jinja 2"

--- Module16/task5[Шаблонизатор Jinja 2].py
+++ Module16/task5[Шаблонизатор Jinja 2].py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.testclient import TestClient
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="Module16/templates")
 
 
 class User(BaseModel):
